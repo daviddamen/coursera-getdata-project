ea81fceb908d0411624cd8cfc005b4704fa79b9e
David Damen <david.damen@arguslabs.be>
2014-06-17 17:40:46 +0200

    Implemented creation of tidy dataset. Updated readme-file to describe the process for coming up with the tidy dataset.

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,51 @@
 #
-# Initialize variables and data set
+# Initialize variables and data set.
 #
 data_origin <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 data_destination <- "UCI HAR Dataset.zip"
-data_dir <- "UCI HAR Dataset"
+data_dir <- "UCI HAR Dataset/"
 
 if (!file.exists(data_dir)) {
   download.file(data_origin, destfile=data_destination, method="curl")
   unzip(data_destination)
 }
+
+#
+# Read in the base data that will be merged.
+#
+features <- read.table(paste0(data_dir, "features.txt"), 
+                       col.names=c("id", "label"))
+
+measurements_test <- read.table(paste0(data_dir, "test/X_test.txt"), 
+                                col.names=features$label)
+measurements_train <- read.table(paste0(data_dir, "train/X_train.txt"), 
+                                 col.names=features$label)
+
+activity_labels <- read.table(paste0(data_dir, "activity_labels.txt"),
+                         col.names=c("id", "label"))
+
+activity_test <- read.table(paste0(data_dir, "test/y_test.txt"),
+                            col.names=c("index"))
+activity_train <- read.table(paste0(data_dir, "train/y_train.txt"),
+                             col.names=c("index"))
+
+#
+# The two measurement data frames are first combined, then filtered such that
+# only means and standard deviations of the measurements are retained.
+#
+measurements <- rbind(measurements_test, measurements_train)
+measurements <- measurements[, grep("mean|std", names(measurements))]
+
+#
+# The two reported activity data frames are also combined and their values
+# mapped to factors with descriptive names.
+#
+activity <- rbind(activity_test, activity_train)
+activity$label <- apply(activity, 1, function(x) activity_labels$label[x])
+
+#
+# Both the measuremens and reported activities are merged into a single tidy 
+# data frame.
+#
+tidy <- cbind(measurements, activity$label)
+
